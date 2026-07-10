124503ce486de6b5eaf5666b97554d0e6ae994ea
Dwayne Rademeyer <dwaynerademeyer@gmail.com>
2026-07-10 20:55:16 +0200

    Added floating WhatsApp button

--- products.js
+++ products.js
@@ -1,40 +1,57 @@
 const products = [
 
-    {
-        id: 1,
-        name: "Chocolate Cupcakes",
-        price: 35,
-        image: "Images/IMG-20260602-WA0023.jpg",
-        description: "Soft chocolate cupcakes",
-        category: "Chocolate"
-    },
+{
+    id:1,
+    name:"Chocolate Cupcakes",
+    price:35,
+    image:"Images/IMG-20260602-WA0023.jpg",
+    description:"Soft chocolate cupcakes",
+    category:"Chocolate"
+},
 
-    {
-        id: 2,
-        name: "Apricots",
-        price: 25,
-        image: "Images/Apricots.png",
-        description: "200g dried apricots",
-        category: "Dried Fruit"
-    },
+{
+    id:2,
+    name:"Apricots",
+    price:25,
+    image:"Images/Apricots.png",
+    description:"Sweet dried apricots 200g",
+    category:"Apricots"
+},
 
-    {
-        id: 3,
-        name: "Toffees",
-        price: 25,
-        image: "Images/Toffees.png",
-        description: "Sweet chewy toffees",
-        category: "Candy"
-    },
+{
+    id:3,
+    name:"Toffees",
+    price:25,
+    image:"Images/Toffees.png",
+    description:"Soft chewy sweet toffees",
+    category:"Toffees"
+},
 
-    {
-        id: 4,
-        name: "Marshmallows",
-        price: 25,
-        image: "Images/Marshmallows.png",
-        description: "Soft fluffy marshmallows",
-        category: "Marshmallows"
-    }
+{
+    id:4,
+    name:"Pink & White Marshmallows",
+    price:25,
+    image:"Images/Marshmallows.png",
+    description:"Soft fluffy marshmallows",
+    category:"Marshmallows"
+},
+
+{
+    id:5,
+    name:"Mixed Candy Box",
+    price:50,
+    image:"Images/IMG-20260602-WA0028.jpg",
+    description:"A delicious mix of colourful sweets",
+    category:"Candy"
+},
+
+{
+    id:6,
+    name:"Sweet Surprise Box",
+    price:50,
+    image:"Images/IMG-20260602-WA0029.jpg",
+    description:"A box filled with sweet treats",
+    category:"Candy"
+}
 
 ];
-
